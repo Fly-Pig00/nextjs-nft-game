7249c19a06867dc0b93aa0e804aca0dd5af5f31f
Ehtan <ethanmartin.0508@gmail.com>
2022-11-07 06:46:26 +0900

    store tab transition

--- pages/quest.js
+++ pages/quest.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
 import Link from "next/link";
-
+import { useState } from "react";
 import Grid from "@mui/material/Unstable_Grid2";
 import Header from "../components/header";
 import AnimatedPage from "../components/AnimatedPage";
 import LinearProgress from "@mui/material/LinearProgress";
 
 export default function Quest() {
+  const [isActiveTab, setIsActiveTab] = useState(true);
+
+  const onTabClick = () => {
+    setIsActiveTab(!isActiveTab);
+  };
+
   return (
     <AnimatedPage>
       <Header />
@@ -30,40 +36,64 @@ export default function Quest() {
               <img src="/image/div.svg" className="divider"></img>
               <h3>Available ( 8 )</h3>
               <div>
-                <a className="daily active">DAILY</a>
-                <a className="weekly passive">WEEKLY</a>
+                <a
+                  className={isActiveTab ? "active" : "passive"}
+                  onClick={() => onTabClick()}
+                >
+                  DAILY
+                </a>
+                <a
+                  className={isActiveTab ? "passive" : "active"}
+                  onClick={() => onTabClick()}
+                >
+                  WEEKLY
+                </a>
               </div>
               <img width="320px" className="divider2" src="image/div.png" />
             </div>
           </Grid>
           <Grid container className="store-items">
-            <div className="store-item">
-              <img className="img" src="image/items/40.png" />
-              <h2>sickle</h2>
-              <h3>Earn them by creating topics in the community forums.</h3>
-              <LinearProgress
-                color="primary"
-                variant="determinate"
-                value={60}
-                style={{ height: "10px", width: "80px", borderRadius: "10px" }}
-              />
-              <h4>320/500</h4>
-              <span class="badge css-106c1u2-MuiBadge-badge">+ 10 xp</span>
-            </div>
-            <div className="store-item">
-              <img className="img" src="image/items/52.png" />
-              <h2>Armour</h2>
-              <h3>Earn them by creating topics in the community forums.</h3>
-              <LinearProgress
-                color="primary"
-                variant="determinate"
-                value={60}
-                style={{ height: "10px", width: "80px", borderRadius: "10px" }}
-              />
+            {isActiveTab ? (
+              <>
+                <div className="store-item">
+                  <img className="img" src="image/items/40.png" />
+                  <h2>sickle</h2>
+                  <h3>Earn them by creating topics in the community forums.</h3>
+                  <LinearProgress
+                    color="primary"
+                    variant="determinate"
+                    value={60}
+                    style={{
+                      height: "10px",
+                      width: "80px",
+                      borderRadius: "10px",
+                    }}
+                  />
+                  <h4>320/500</h4>
+                  <span class="badge css-106c1u2-MuiBadge-badge">+ 10 xp</span>
+                </div>
+                <div className="store-item">
+                  <img className="img" src="image/items/52.png" />
+                  <h2>Armour</h2>
+                  <h3>Earn them by creating topics in the community forums.</h3>
+                  <LinearProgress
+                    color="primary"
+                    variant="determinate"
+                    value={60}
+                    style={{
+                      height: "10px",
+                      width: "80px",
+                      borderRadius: "10px",
+                    }}
+                  />
 
-              <h4>320/500</h4>
-              <span class="badge css-106c1u2-MuiBadge-badge">+ 10 xp</span>
-            </div>
+                  <h4>320/500</h4>
+                  <span class="badge css-106c1u2-MuiBadge-badge">+ 10 xp</span>
+                </div>
+              </>
+            ) : (
+              ""
+            )}
             <div className="store-item">
               <img className="img" src="image/items/76.png" />
               <h2>charm</h2>
