fb502057fea2fd399fc2b2009380b77421197023
Ehtan <ethanmartin.0508@gmail.com>
2022-11-07 03:34:57 +0900

    merged

--- components/header.js
+++ components/header.js
@@ -3,39 +3,17 @@ import Login from "../components/login";
 import QuestSidebar from "../components/questsidebar";
 import Link from "next/link";
 import React, { useState, useEffect } from 'react';
+import { Button, Drawer, Box } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import MobileMenu from "./mobilemenu";
 
 export default function Header() {
-
+  const matches = useMediaQuery('(min-width:900px)');
+  
   return (
     <header>
-      {/* <div className="left">
-        <div className="logo">
-          <Link href="/home">
-            <img src="/image/prime-nine_mark.svg" />
-          </Link>
-        </div>
-        <div class="navbar">
-          <Link href="/content">About</Link>
-          <div class="dropdown">
-            <button class="dropbtn">Game</button>
-            <div class="dropdown-content">
-              <Link href="/roadmap">Roadmap</Link>
-              <Link href="/content2">Tokenomics</Link>
-              <Link href="/team">Team</Link>
-              <Link href="/omnichain">Omnichain</Link>
-            </div>
-          </div>
-          <Link href="/mint">Mint</Link>
-          <Link href="/calculator">Calculator</Link>
-
-        </div>
-      </div>
-      <div className="right">
-        <Dashboard />
-        <QuestSidebar />
-      </div> */}
       <div className="header-container">
-        <div className="flex">
+        <div className="flex menu-navbar">
           <div className="logo">
             <Link href="/home">
               <img src="/image/prime-nine_mark.svg" />
@@ -55,8 +33,11 @@ export default function Header() {
             <Link href="/mint">Mint</Link>
             <Link href="/calculator">Calculator</Link>
           </div>
+          <div className="hamburger-button">
+            <MobileMenu />
+          </div>
         </div>
-        <div className="flex">
+        <div className="flex app-navbar">
           <Dashboard />
           <QuestSidebar />
         </div>
