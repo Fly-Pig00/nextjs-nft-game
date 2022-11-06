87572b01d8bd38da150782ef3ab4f413322a6f42
Yanming Michel <108870761+marlborocool@users.noreply.github.com>
2022-11-06 11:36:45 -0700

    Update mobilemenu.js
    Mobile responsive

--- components/mobilemenu.js
+++ components/mobilemenu.js
@@ -5,10 +5,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import StarBorder from '@mui/icons-material/StarBorder';
 
 export default function QuestSidebar() {
   const [state, setState] = useState({ right: false });
